78febf0da27204a4d87894a5da4104e6b8995095
ramm4816 <web5talker@yandex.ru>
2022-12-29 23:22:49 +0700

    commit

--- session_maker.py
+++ session_maker.py
@@ -1,9 +1,10 @@
-from pyrogram import Client, filters
+from pyrogram import Client
+import uuid
 
 api_id = 20886214
 api_hash = "ba51cbd8e8f1dd0fce0d755ce0970600"
 
-#for number in range(10,16):
-pycl = Client(f"s10", api_id, api_hash)
+pycl = Client(str(uuid.uuid4()), api_id, api_hash)
 pycl.start()    
 pycl.stop()
+
